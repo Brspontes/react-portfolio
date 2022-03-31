01cfffb4455638eaafb0255f63a59ecb5bf4bef2
Brian Pontes <brian.robert16@hotmail.com>
2022-03-31 16:56:06 -0300

    implementando theme chackra ui

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Header from './components/header';
+import { ChakraProvider  } from '@chakra-ui/react'
+import { theme } from './theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Header></Header>
-    <App />
+    <ChakraProvider theme= { theme }>
+      <Header />
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
