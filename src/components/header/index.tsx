44edb680f23ec725d562b577fb2d379d7d200ea7
Brian Pontes <brian.robert16@hotmail.com>
2022-04-07 09:13:42 -0300

    modificando cor header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -5,25 +5,28 @@ import './header.css'
 function Header () {
   const { colorMode, toggleColorMode } = useColorMode()
   return (
-    <Flex as='header' width="100%" maxWidth={1480} height="120" marginX="auto" marginTop="4" paddingX="6" align="center" justifyContent="space-between" fontSize={["1rem","1.2rem"]}>
-      <Box>
-        <p>BR.</p>
-      </Box>
-      <Flex direction="row-reverse">
-        <nav>
-          <ul>
-            <li>Projetos</li>
-            <li>Resume</li>
-            <li>Contato</li>
-            <li>
-              {colorMode === 'light' ? 
-                <Button onClick={toggleColorMode}><MoonIcon /></Button> :
-                <Button onClick={toggleColorMode}><SunIcon /></Button>
-              }
-            </li>
-          </ul>
-        </nav>
-      </Flex>  
+    <Flex bgColor={colorMode === 'light' ? '#F5F5F5' : '#171c26'}>
+      <Flex as='header' width="100%" maxWidth={1480} height="120" marginX="auto" 
+        paddingX="6" align="center" justifyContent="space-between" fontSize={["1rem","1.2rem"]}>
+          <Box>
+            <p>BR.</p>
+          </Box>
+          <Flex direction="row-reverse">
+            <nav>
+              <ul>
+                <li>Projetos</li>
+                <li>Resume</li>
+                <li>Contato</li>
+                <li>
+                  {colorMode === 'light' ? 
+                    <Button onClick={toggleColorMode}><MoonIcon /></Button> :
+                    <Button onClick={toggleColorMode}><SunIcon /></Button>
+                  }
+                </li>
+              </ul>
+            </nav>
+          </Flex>  
+      </Flex>
     </Flex>
   )
 }
