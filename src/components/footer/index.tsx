64aea9993d5c2a568c1ad27453614ae1b6d0da48
Brian Pontes <brian.robert16@hotmail.com>
2022-04-19 21:13:49 -0300

    links em nova tab

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -5,9 +5,9 @@ export default function Footer() {
   return (
     <Flex alignItems='center' justifyContent='center' borderTop='1px' width="100%" maxWidth={1360} height='240' marginX="auto" >
       <Flex width='64' justifyContent='space-between'>
-        <Box><Link href='https://www.linkedin.com/in/brian-pontes-420675a4/'><AiFillLinkedin size='50'/></Link></Box>
-        <Box><Link href='https://www.instagram.com/__brianrobert/'><AiOutlineInstagram size='50'/></Link></Box>
-        <Box><Link href='mailto:brian.robert16@hotmail.com'><AiOutlineMail size='50'/></Link></Box>
+        <Box><Link href='https://www.linkedin.com/in/brian-pontes-420675a4/' isExternal><AiFillLinkedin size='50'/></Link></Box>
+        <Box><Link href='https://www.instagram.com/__brianrobert/' isExternal><AiOutlineInstagram size='50'/></Link></Box>
+        <Box><Link href='mailto:brian.robert16@hotmail.com' isExternal><AiOutlineMail size='50'/></Link></Box>
       </Flex>
     </Flex>
   )
